09c8a77fea77cf2aca36f0ab740fee7a3a6537c2
Yan <yangabriel2012@gmail.com>
2022-12-19 21:59:37 -0300

    feat: implementando entrada de dados

---
+++ src/components/Entry/index.tsx
@@ -0,0 +1,21 @@
+import { text } from "stream/consumers"
+
+interface EntryInterface{
+    text: string,
+    type: 'text' | 'number',
+    value?: string | number,
+    onlyReading?: boolean
+    handleChange?: (value: any) => void
+}
+export const Entry = ({text, type, value, onlyReading, handleChange}:EntryInterface) => {
+    return(
+        <div className="flex flex-col">
+            <label className="mb-4">
+                {text}
+            </label>
+            <input className={`border border-blue-500 rounded-1g focus:outline-none bg-gray-50 px-4 py-2 ${onlyReading ? ' ' : 'bg-white'}` }type={type ?? 'text'} value={value} readOnly={onlyReading}
+            onChange={event => handleChange?.(event.target.value)}
+            />
+        </div>
+    )
+}
